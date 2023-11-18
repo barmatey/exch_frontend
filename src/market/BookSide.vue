8c9e6d74e1fefd40a16c94f7995e5f7afa5de51d
barma <barmatey@gmail.com>
2023-11-18 14:16:10 +0300

    processing

--- src/market/BookSide.vue
+++ src/market/BookSide.vue
@@ -1,19 +1,36 @@
 <template>
     <div>
-        <div class="order-book-side" style="border-right: 1px solid var(--ui-main-color-primary-hover)">
+        <div class="level2-header">
             <div class="side-title">
                 {{ title }}
             </div>
-            <div class="side-level" v-for="level in orderBookSide">
-                <div class="price-part">
+            <div class="level2-grid-wrapper header-part">
+                <div class="table-item">
+                    Price
+                </div>
+                <div class="table-item">
+                    Quantity
+                </div>
+                <div class="table-item">
+                    Amount
+                </div>
+            </div>
+        </div>
+        <div class="level2-content">
+            <div class="level2-grid-wrapper" v-for="level in orderBookSide">
+                <div class="order-info-item">
                     {{ level[0] }}
                 </div>
-                <div class="quantity-part">
+                <div class="order-info-item">
                     {{ level[1] }}
+                </div>
+                <div class="order-info-item">
+                    {{ level[0] * level[1] }}
                 </div>
             </div>
         </div>
     </div>
+
 </template>
 
 <script setup lang="ts">
@@ -27,26 +44,35 @@ const p = defineProps<{
 
 <style scoped>
 
-.order-book-side {
-    width: 300px;
-    height: 100%;
-    padding: 6px 12px;
+.level2-header {
+    border-bottom: 1px solid black;
+    padding: 0 12px;
+}
+
+.level2-content {
+    padding: 0 12px;
 }
 
 .side-title {
-
+    margin-bottom: 6px;
+    font-size: 18px;
 }
 
-.side-level {
-    display: flex;
+.level2-grid-wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
 }
 
-.price-part {
-    width: 150px;
+.header-part{
+    margin-bottom: 3px;
 }
 
-.quantity-part {
-    width: 150px;
+.table-item {
+    font-size: 14px;
+}
+
+.order-info-item {
+    margin-top: 3px;
 }
 
 </style>
