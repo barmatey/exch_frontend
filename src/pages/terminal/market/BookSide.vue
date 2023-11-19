052eb27b920a6b9fcdad5f8e319ad81970785f93
barma <barmatey@gmail.com>
2023-11-19 12:47:31 +0300

    processing

--- src/pages/terminal/market/BookSide.vue
+++ src/pages/terminal/market/BookSide.vue
@@ -1,33 +1,15 @@
 <template>
-    <div>
-        <div class="level2-header">
-            <div class="side-title">
-                {{ title }}
-            </div>
-            <div class="level2-grid-wrapper header-part">
-                <div class="table-item">
-                    Price
-                </div>
-                <div class="table-item">
-                    Quantity
-                </div>
-                <div class="table-item">
-                    Amount
-                </div>
-            </div>
-        </div>
-        <div class="level2-content">
-            <div class="level2-grid-wrapper" v-for="level in orderBookSide">
-                <div class="order-info-item">
-                    {{ level[0] }}
-                </div>
-                <div class="order-info-item">
-                    {{ level[1] }}
-                </div>
-                <div class="order-info-item">
-                    {{ level[0] * level[1] }}
-                </div>
-            </div>
+    <div class="bs-grid-wrapper">
+        <div>Price</div>
+        <div>Quantity</div>
+        <div>Amount</div>
+        <div
+            v-for="[price, quantity] in orderBookSide.entries()"
+            style="display: contents"
+        >
+            <div>{{ price }}</div>
+            <div>{{ quantity }}</div>
+            <div>{{ price * quantity }}</div>
         </div>
     </div>
 
@@ -40,39 +22,12 @@ const p = defineProps<{
     orderBookSide: Map<number, number>,
     title: string,
 }>()
+
 </script>
 
 <style scoped>
-
-.level2-header {
-    border-bottom: 1px solid black;
-    padding: 0 12px;
-}
-
-.level2-content {
-    padding: 0 12px;
-}
-
-.side-title {
-    margin-bottom: 6px;
-    font-size: 18px;
-}
-
-.level2-grid-wrapper {
+.bs-grid-wrapper {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
 }
-
-.header-part{
-    margin-bottom: 3px;
-}
-
-.table-item {
-    font-size: 14px;
-}
-
-.order-info-item {
-    margin-top: 3px;
-}
-
 </style>
--- src/pages/terminal/market/OBook.vue
+++ src/pages/terminal/market/OBook.vue
@@ -3,10 +3,7 @@
         <div class="order-book-window">
             <book-side :orderBookSide="orderBook.buyLevel" title="Buy offers"/>
             <book-side :orderBookSide="orderBook.sellLevel" title="Sell offers"/>
-        </div>
-        <div class="new-order-window">
-            <new-order :orderBookGateway="gateway" side="Buy"/>
-            <new-order :orderBookGateway="gateway" side="Sell"/>
+            <div></div>
         </div>
     </div>
 
@@ -29,28 +26,17 @@ const p = defineProps<{
 
 <style scoped>
 .order-book {
-    color: var(--ui-main-color-white);
 }
 
 .order-book-window {
-    width: max-content;
-    height: 400px;
-    background: var(--ui-main-color-primary);
-    display: flex;
-    border: 1px solid var(--ui-main-color-primary-hover);
-    border-top-left-radius: 6px;
-    border-top-right-radius: 6px;
+    border: 1px solid black;
+    border-radius: 12px;
+    display: grid;
+    grid-template-columns: 300px 300px 150px;
 }
 
 .new-order-window {
-    width: max-content;
-    height: max-content;
-    background: var(--ui-main-color-primary);
-    display: flex;
-    align-items: center;
-    border: 1px solid var(--ui-main-color-primary-hover);
-    border-bottom-left-radius: 6px;
-    border-bottom-right-radius: 6px;
+
 }
 
 </style>
