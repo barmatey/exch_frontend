6a8e54762bda06ad9d07f71d5597a20a6b654e41
barma <barmatey@gmail.com>
2023-12-01 18:34:21 +0300

    auth

--- src/pages/terminal/TeminalPage.vue
+++ src/pages/terminal/TeminalPage.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="terminal terminal-grid-wrapper">
-<!--        <commodity-window class="commodity-window" @onSelect="(x) => handleCommodityChanged(x)"/>-->
+        <commodity-window class="commodity-window" @onSelect="(x) => handleCommodityChanged(x)"/>
         <div>
-<!--            <level-two :ticker="ticker"/>-->
+            <level-two :ticker="ticker"/>
             <create-order :ticker="ticker"/>
-<!--            <transaction-list :ticker="ticker" style="height: calc(100vh - 614px); margin-top: 24px;"/>-->
+            <transaction-list :ticker="ticker" style="height: calc(100vh - 614px); margin-top: 24px;"/>
 
         </div>
         <div>
             <order-list style="height: calc(50vh - 80px)"/>
-<!--            <deal-list style="height: calc(50vh - 80px); margin-top: 24px"/>-->
+            <deal-list style="height: calc(50vh - 80px); margin-top: 24px"/>
         </div>
     </div>
 </template>
